56c460766558adbf506aaeb44eae4b90c1f5371c
dasgoutam <goutam2475@gmail.com>
2024-11-07 17:35:58 +0100

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-            I hope this works
-        </p>
-      </header>
+        <Header />
     </div>
   );
 }
